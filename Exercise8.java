b618039c209e90a814c53668408785cc119de9ec
TuanDao-0110 <rickytuan1991@gmail.com>
2023-11-02 16:53:43 +0200

    exercise 9

---
+++ Exercise8.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Exercise8 {
+
+    public Exercise8() {
+        // TODO Auto-generated constructor stub
+    }
+
+    final static int min = -50;
+    final static int max = 50;
+
+    // Input n:
+    public static int inputN(Scanner scan) {
+        int n;
+        do {
+            System.out.println("Enter a value > 1: ");
+            n = Integer.parseInt(scan.nextLine());
+        } while (n < 1);
+        return n;
+    }
+
+    // Create a 2D array:
+    public static int[][] createArray(int numRows, int numCols, Scanner scan) {
+        int[][] a = new int[numRows][numCols];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                a[i][j] = min + (int) (Math.random() * ((max - min) + 1));
+            }
+        }
+        return a;
+    }
+
+    // Display a 2D array:
+    public static void displayArray(int a[][], int numRows, int numCols) {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                System.out.print(a[i][j] + "\t");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    // Calculate the sum of values in each row:
+    public static void sumEachRow(int a[][], int numRows, int numCols) {
+        int sum = 0;
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                sum += a[i][j];
+            }
+            System.out.println("Sum of integers in row " + i + " is: " + sum);
+            sum = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int numRows = inputN(scan);
+        System.out.println("Enter the number of columns:");
+        int numCols = inputN(scan);
+        int a[][] = createArray(numRows, numCols, scan);
+        System.out.println("Array A values are: ");
+        displayArray(a, numRows, numCols);
+        sumEachRow(a, numRows, numCols);
+    }
+}
--- README.md
+++ README.md
@@ -8,3 +8,14 @@
 - Find the largest negative number
 ## Exercise 6:
 - Check whether `2D array` contains negative number ?
+## Exercise 7:
+- Check if a column in the `matrix` is decreasing
+## Exercise 8:
+- Sum each `row` in the `2D array`
+```
+Sum of integers in row 0 is: 32
+Sum of integers in row 1 is: -41
+Sum of integers in row 2 is: 9
+Sum of integers in row 3 is: 79
+```
+## Exercise 9:
